9fc5d834a1ea15111aa621295000de5696e395c9
azimin <azimin@team.amocrm.com>
2023-11-16 02:32:01 +0300

    feat(front): Сверстаны страницы логина и регистрации.

--- resources/js/Pages/Register.vue
+++ resources/js/Pages/Register.vue
@@ -3,7 +3,17 @@
 </script>
 
 <template>
-
+    <v-container>
+        <v-row>
+            <v-col offset-md="2" md="8" offset-lg="3" lg="6">
+                <h2 class="text-h2 mb-3">Register</h2>
+                <v-text-field label="Nickname" />
+                <v-text-field type="password" label="Password" />
+                <v-text-field type="password" label="Confirm password" />
+                <v-btn color="primary">Register</v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <style scoped>
