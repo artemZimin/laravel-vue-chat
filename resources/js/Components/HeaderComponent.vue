5b8d3842e02199ef04c9532f9afbbc68c1183152
azimin <azimin@team.amocrm.com>
2023-11-20 04:31:10 +0300

    feat(front): Сверстал кнопку выхода из аккаунта

--- resources/js/Components/HeaderComponent.vue
+++ resources/js/Components/HeaderComponent.vue
@@ -7,6 +7,7 @@
         <template v-slot:append>
             <v-btn icon="mdi-brightness-7"></v-btn>
             <v-btn icon="mdi-account"></v-btn>
+            <v-btn icon="mdi-logout"></v-btn>
         </template>
     </v-app-bar>
 </template>
